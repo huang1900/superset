fd1667218155fff36d60651c0929654f19eb825d
huang1900 <714743238@qq.com>
2017-06-28 18:55:06 +0800

    Merge branch '18.5.3'
    # Conflicts:
    #	superset/assets/package.json

--- superset/viz.py
+++ superset/viz.py
@@ -209,11 +209,14 @@ class BaseViz(object):
     @property
     def cache_key(self):
         query_obj = self.query_obj()
+        s = ""
         try:
-          key=self.datasource.get_query_str(query_obj)
+          s = self.datasource.get_query_str(query_obj)
         except Exception as e:
             logging.error("查询错误:" +utils.error_msg_from_exception(e))
-        return hashlib.md5(key.encode('utf-8')).hexdigest()
+        if len(s)==0:
+            s = str([(k, self.form_data[k]) for k in sorted(self.form_data.keys())])
+        return hashlib.md5(s.encode('utf-8')).hexdigest()
 
     def is_timeout(self,payload):
          if payload and payload['cached_dttm']:
