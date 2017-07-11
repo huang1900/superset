33827e5e34187130d3959cf4b679042cf63be935
huang1900 <714743238@qq.com>
2017-07-11 18:03:50 +0800

    合并master

--- superset/assets/javascripts/explore/stores/controls.jsx
+++ superset/assets/javascripts/explore/stores/controls.jsx
@@ -7,11 +7,17 @@ import ColumnOption from '../../components/ColumnOption';
 const D3_FORMAT_DOCS = 'D3 format syntax: https://github.com/d3/d3-format';
 
 // input choices & options
-const D3_TIME_FORMAT_OPTIONS = [
+const D3_FORMAT_OPTIONS = [
   [',', '千位分隔'],
   ['.2%', '百分比（保留2位）'],
 ];
-
+export const D3_TIME_FORMAT_OPTIONS = [
+    ['smart_date', '自动匹配'],
+    ['%m/%d/%Y', '%m/%d/%Y | 01/14/2019'],
+    ['%Y-%m-%d', '%Y-%m-%d | 2019-01-14'],
+    ['%Y-%m-%d %H:%M:%S', '%Y-%m-%d %H:%M:%S | 2019-01-14 01:32:10'],
+    ['%H:%M:%S', '%H:%M:%S | 01:32:10'],
+];
 const ROW_LIMIT_OPTIONS = [10, 50, 100, 250, 500, 1000, 5000, 10000, 50000];
 
 const SERIES_LIMITS = [0, 5, 10, 25, 50, 100, 500];
