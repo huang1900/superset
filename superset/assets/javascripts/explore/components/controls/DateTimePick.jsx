6aed39d4ee645b49f32b626db2f58ea9b9c2219f
huang1900 <714743238@qq.com>
2017-07-19 14:56:25 +0800

    时间控件

--- superset/assets/javascripts/explore/components/controls/DateTimePick.jsx
+++ superset/assets/javascripts/explore/components/controls/DateTimePick.jsx
@@ -1,70 +1,119 @@
-import 'flatpickr/dist/themes/airbnb.css'
-import Flatpickr from 'react-flatpickr'
-import { Component } from 'react'
-import React from 'react';
-import PropTypes from 'prop-types';
 
-const zh=require('flatpickr/dist/l10n/zh.js').zh;
-const propTypes = {
-    time_24hr:PropTypes.bool,
-    weekNumbers:PropTypes.bool,
-    enableTime:PropTypes.bool,
-    enableSeconds:PropTypes.bool,
-    defaultHour:PropTypes.number,
-    onChange: PropTypes.func,
-    defaultMinute:PropTypes.number,
-};
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Flatpickr from 'flatpickr'
 
-const defaultProps = {
-    time_24hr:true,
-    weekNumbers:false,
-    enableTime:false,
-    onChange: () => {},
-    enableSeconds:false,
-    defaultHour:12,
-    defaultMinute:0,
-};
-export default class DateTimePick extends Component {
-  constructor(props) {
-    super(props);
-    this.onChange = this.onChange.bind(this);
-  }
-  onChange(event,value) {
-      console.log(event)
-      console.log(value)
-      this.props.onChange(event, []);
+const hooks = [
+    'onChange',
+    'onOpen',
+    'onClose',
+    'onMonthChange',
+    'onYearChange',
+    'onReady',
+    'onValueUpdate',
+    'onDayCreate'
+]
+
+class DateTimePicker extends Component {
+    static propTypes = {
+        defaultValue: PropTypes.string,
+        options: PropTypes.object,
+        onChange: PropTypes.func,
+        onOpen: PropTypes.func,
+        onClose: PropTypes.func,
+        onMonthChange: PropTypes.func,
+        onYearChange: PropTypes.func,
+        onReady: PropTypes.func,
+        onValueUpdate: PropTypes.func,
+        onDayCreate: PropTypes.func,
+        value: PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.array,
+            PropTypes.object,
+            PropTypes.number
+        ]),
+        children: PropTypes.node
     }
-  componentDidMount() {
 
-  }
+    static defaultProps = {
+        options: {}
+    }
 
+    componentWillReceiveProps(props) {
+        const { options } = props
 
-  render() {
-      const option={
-          time_24hr:this.props.time_24hr,
-          enableTime:this.props.enableTime,
-          enableSeconds:this.props.enableSeconds,
-          defaultHour:this.props.defaultHour,
-          defaultMinute:this.props.defaultMinute,
-          locale:zh,
-          wrap: true,
-      }
-     return (
-        <Flatpickr
-        <input type="text" placeholder="Select Date.." data-input>
-            <a class="input-button" title="toggle" data-toggle>
-                <i class="icon-calendar"></i>
-            </a>
-            <a class="input-button" title="clear" data-clear>
-                <i class="icon-close"></i>
-            </a>
-            onChange={this.onChange}
-            className="Select-control"
-            options={option}
-        />
-      )
-  }
+        if (props.hasOwnProperty('value')) {
+            this.flatpickr.setDate(props.value, false)
+        }
+
+        // Add prop hooks to options
+        for (let hook of hooks) {
+            if (props[hook]) {
+                options[hook] = props[hook]
+            }
+        }
+
+        const optionsKeys = Object.getOwnPropertyNames(props.options)
+
+        for (let index = optionsKeys.length - 1; index >= 0; index--) {
+            const key = optionsKeys[index]
+            let value = props.options[key]
+
+            // Hook handlers must be set as an array
+            if (hooks.indexOf(key) !== -1 && !Array.isArray(value)) {
+                value = [value]
+            }
+
+            this.flatpickr.set(key, value)
+        }
+    }
+
+    componentDidMount() {
+        const options = {
+            onClose: () => {
+                this.node.blur && this.node.blur()
+            },
+            ...this.props.options
+        }
+
+        // Add prop hooks to options
+        for (let hook of hooks) {
+            if (this.props[hook]) {
+                options[hook] = this.props[hook]
+            }
+        }
+
+        this.flatpickr = new Flatpickr(this.node, options)
+
+        if (this.props.hasOwnProperty('value')) {
+            this.flatpickr.setDate(this.props.value, false)
+        }
+    }
+
+    componentWillUnmount() {
+        this.flatpickr.destroy()
+    }
+
+    render() {
+        // eslint-disable-next-line no-unused-vars
+        const { options, defaultValue, value, children, ...props } = this.props
+
+        // Don't pass hooks to dom node
+        for (let hook of hooks) {
+            delete props[hook]
+        }
+
+        return options.wrap
+            ? (
+                <div {...props} ref={node => { this.node = node }}>
+                    { children }
+                </div>
+            )
+            : (
+                <input {...props} defaultValue={defaultValue}
+                       ref={node => { this.node = node }} />
+            )
+    }
 }
 
-DateTimePick.propTypes = propTypes;
-DateTimePick.defaultProps = defaultProps;
+export default DateTimePicker
